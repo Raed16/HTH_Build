71ecdf917880aef6538369568a3e5ff40348628f
raed16 <raedjahmed@gmail.com>
2022-06-30 13:39:51 -0700

    first commit

---
+++ Getting-Started-with-Python-Raed23 (1)/about_me.py
@@ -0,0 +1,25 @@
+full_name = "Raed Ahmed"
+nickname = "rara"
+age = 17
+python_experience = True
+
+hobbies = [
+    "painting", "playing basketball", "watching shows", "hiking",
+    "camping", "playing video games"
+]
+
+favorites = {
+    "movie": "lion",
+    "number": 21,
+    "food": "pasta",
+    "hobby": hobbies[0],
+    "game": "COD",
+}
+
+print(full_name, nickname, age, python_experience)
+print()
+print(hobbies)
+print()
+print(favorites)
+
+all_vars = dict(vars())
